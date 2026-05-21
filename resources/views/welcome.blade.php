c40e276561346c54c0157ae881f1a0e9e70392f0
Emmanuel Damian <emmanueldamian@MacBook-Pro-de-Emmanuel.local>
2026-05-21 11:50:29 -0600

    Subo cambios moviemdo el app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,12 +43,12 @@
     <div id="app">
         @yield('content')
     </div>
-    
+    <script src="{{ asset('js/app.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
+
 </body>
 </html>
